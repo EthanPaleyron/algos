ccf45f4938dd70bd1449287030cc20388c5999e0
SoldatdOR05 <epaleyron@edenschool.fr>
2023-06-16 16:11:54 +0200

    second smallest

---
+++ algos_JS/codes/second-smallest.js
@@ -0,0 +1,17 @@
+// 🔴Retourner le 2 ème chiffre plus petit
+// Code is => INCOMPLETE❌
+
+function secondSmallest(array) {
+    let newArray = [];
+    if (condition) {
+        
+    }
+    // for (let i = 0; i < array.length; i++) {
+    //     newArray += array[i];
+    //     if (array[i] < newArray) {
+    //     }
+    // }
+    return newArray;
+}
+
+console.log(secondSmallest([4, 6, 2, 5, 11, 9, 3, 8]));
