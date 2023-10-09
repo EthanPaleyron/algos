ab53db923167327db1d094619906fbc75e445e51
Ethan <unclebigbay@hashnode.com>
2023-10-09 13:30:06 +0200

    algos diviseurs

---
+++ JS/Math/divisibilite.js
@@ -0,0 +1,15 @@
+function diviseurs(n) {
+  let i = 2;
+  let divs = "1 ";
+  let some = 0;
+  while (i <= n) {
+    if (n % i == 0) {
+      divs = divs + i + " ";
+      some += i;
+    }
+    i++;
+  }
+  return `Les diviseurs de ${n} sont => ${divs}\nla some de tout les diviseurs est de => ${some}`;
+}
+
+console.log(diviseurs(66));
