50be321e42c9e88b9907cc44feb07c0399bb52a5
Ethan <epaleyron@edenschool.fr>
2024-01-16 10:18:14 +0100

    nouvel fonction

--- JS/Math/fonction.js
+++
@@ -1,4 +0,0 @@
-function f(n) {
-  return 2 * n + 5;
-}
-console.log(f(5));
---
+++ JS/Math/formule-de-lorentz.js
@@ -0,0 +1,4 @@
+function formuleDeLorentz(taille) {
+  return taille - 100 - (taille - 150) / 4;
+}
+console.log(formuleDeLorentz(185));
