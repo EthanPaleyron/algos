ff7cc51f8a352cb9786f30bd1064355a8cb309ad
Ethan <epaleyron@edenschool.fr>
2024-03-04 17:16:34 +0100

    mario tainix

---
+++ JS/TAINIX/entrainementPeachMario.js
@@ -0,0 +1,27 @@
+const parcours = "P_P_____P_____P____P_P__P____P_____P____P__P_____P'";
+
+let vide = 0;
+let peach = true;
+let sauts = "";
+for (let i = 0; i < parcours.length; i++) {
+  if (parcours[i] === "P") {
+    if (vide === 1 || vide === 2) {
+      sauts += "M";
+    } else if (vide === 4 || vide === 5) {
+      sauts += "P";
+    } else if (vide === 3) {
+      if (peach) {
+        sauts += "PM";
+        peach = false;
+      } else {
+        sauts += "MP";
+        peach = true;
+      }
+    }
+    vide = 0;
+  } else {
+    vide++;
+  }
+}
+
+console.log(sauts);
